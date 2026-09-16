6e7f94eecc3cf5d5fb3f10aab364c0467c2de853
Justin Underhay <jju115@telus.net>
2026-09-16 13:54:05 -0700

    Type color and styling.

--- frontend/pokemon-tournament/src/app/components/pokemon-battle-card/pokemon-battle-card.component.ts
+++ frontend/pokemon-tournament/src/app/components/pokemon-battle-card/pokemon-battle-card.component.ts
@@ -16,13 +16,33 @@ import {MatButtonModule} from '@angular/material/button';
 })
 export class PokemonBattleCardComponent implements OnInit {
 
+    TYPE_COLORS: Record<string, string> = {
+        normal:   '#A8A878',
+        fire:     '#F08030',
+        water:    '#6890F0',
+        electric: '#F8D030',
+        grass:    '#78C850',
+        ice:      '#98D8D8',
+        fighting: '#C03028',
+        poison:   '#A040A0',
+        ground:   '#E0C068',
+        flying:   '#A890F0',
+        psychic:  '#F85888',
+        bug:      '#A8B820',
+        rock:     '#B8A038',
+        ghost:    '#705898',
+        dragon:   '#7038F8',
+        dark:     '#705848',
+        steel:    '#B8B8D0',
+        fairy:    '#EE99AC',
+      };
+
     @Input() pokemon: Pokemon = new Pokemon();
 
     public spriteUrl: string = '';
     public battleCryUrl: string = '';
 
     public winRate: number = 0;
-
 
     public ngOnInit(): void {
       let dec = this.pokemon.wins / (this.pokemon.wins + this.pokemon.ties + this.pokemon.losses);
